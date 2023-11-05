7b1c696037daec3fbf1522622074afdb4a586f9d
makaya2afonso <makaya2afonso@gmail.com>
2023-11-04 23:55:36 -0300

    rewrite struct tests

--- __tests__/z/struct_test.go
+++ __tests__/z/struct_test.go
@@ -1,10 +1,10 @@
 package z_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/mkafonso/go-schema/z"
-	"github.com/stretchr/testify/assert"
 )
 
 type Person struct {
@@ -21,9 +21,15 @@ func TestParseStructValidData(t *testing.T) {
 	person := Person{}
 	err := z.ParseStruct(data, &person)
 
-	assert.NoError(t, err)
-	assert.Equal(t, "Alice", person.Name)
-	assert.Equal(t, 30, person.Age)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+	if person.Name != "Alice" {
+		t.Errorf("Expected Name to be 'Alice', but got: %s", person.Name)
+	}
+	if person.Age != 30 {
+		t.Errorf("Expected Age to be 30, but got: %d", person.Age)
+	}
 }
 
 func TestParseStructMissingFieldAge(t *testing.T) {
@@ -34,8 +40,9 @@ func TestParseStructMissingFieldAge(t *testing.T) {
 	person := Person{}
 	err := z.ParseStruct(data, &person)
 
-	assert.Error(t, err)
-	assert.Equal(t, err.Error(), "missing field: age")
+	if err == nil || err.Error() != "missing field: age" {
+		t.Errorf("Expected 'missing field: age' error, but got: %v", err)
+	}
 }
 
 func TestParseStructInvalidFieldTypeAge(t *testing.T) {
@@ -47,8 +54,9 @@ func TestParseStructInvalidFieldTypeAge(t *testing.T) {
 	person := Person{}
 	err := z.ParseStruct(data, &person)
 
-	assert.Error(t, err)
-	assert.Equal(t, err.Error(), "field type mismatch: age")
+	if err == nil || err.Error() != "field type mismatch: age" {
+		t.Errorf("Expected 'field type mismatch: age' error, but got: %v", err)
+	}
 }
 
 func TestParseStructInvalidTarget(t *testing.T) {
@@ -57,10 +65,11 @@ func TestParseStructInvalidTarget(t *testing.T) {
 		"age":  25,
 	}
 
-	// pass a non-pointer target, which should result in an error.
+	// Pass a non-pointer target, which should result in an error.
 	person := Person{}
 	err := z.ParseStruct(data, person)
 
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "target must be a pointer to a struct")
+	if err == nil || !strings.Contains(err.Error(), "target must be a pointer to a struct") {
+		t.Errorf("Expected 'target must be a pointer to a struct' error, but got: %v", err)
+	}
 }
