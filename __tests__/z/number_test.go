01845c2f2547b71fbe8ab369ebde714f2fb7d714
makaya2afonso <makaya2afonso@gmail.com>
2023-11-04 23:49:16 -0300

    rewrite numer tests

--- __tests__/z/number_test.go
+++ __tests__/z/number_test.go
@@ -4,38 +4,58 @@ import (
 	"testing"
 
 	"github.com/mkafonso/go-schema/z"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestNumberSchemaTestValidNumber(t *testing.T) {
 	schema := z.NewNumberSchema()
 	result, err := schema.Parse(42.5)
 
-	assert.Equal(t, 42.5, result)
-	assert.NoError(t, err)
+	if result != 42.5 {
+		t.Errorf("Expected result to be 42.5, got %v", result)
+	}
+	if err != nil {
+		t.Errorf("Expected error to be nil, got %v", err)
+	}
 }
 
 func TestNumberSchemaTestParseWithDefaultErrorMessage(t *testing.T) {
 	schema := z.NewNumberSchema()
 	result, err := schema.Parse("Hi")
 
-	assert.Equal(t, 0.0, result)
-	assert.Error(t, err)
-	assert.Equal(t, err.Error(), "value must be a number")
+	if result != 0.0 {
+		t.Errorf("Expected result to be 0.0, got %v", result)
+	}
+	if err == nil {
+		t.Error("Expected an error, but got nil")
+	} else if err.Error() != "value must be a number" {
+		t.Errorf("Expected error message to be 'value must be a number', got '%s'", err.Error())
+	}
 }
 
 func TestNumberSchemaTestParseWithCustomErrorMessage(t *testing.T) {
 	schema := z.NewNumberSchema()
 	result, err := schema.Parse("Hi", "Custom error message")
 
-	assert.Equal(t, 0.0, result)
-	assert.EqualError(t, err, "Custom error message")
+	if result != 0.0 {
+		t.Errorf("Expected result to be 0.0, got %v", result)
+	}
+	if err == nil {
+		t.Error("Expected an error, but got nil")
+	} else if err.Error() != "Custom error message" {
+		t.Errorf("Expected error message to be 'Custom error message', got '%s'", err.Error())
+	}
 }
 
 func TestNumberSchemaTestParseWithManyCustomErrorMessages(t *testing.T) {
 	schema := z.NewNumberSchema()
 	result, err := schema.Parse("Hi", "First custom error message", "Second custom error message")
 
-	assert.Equal(t, 0.0, result)
-	assert.Equal(t, err.Error(), "First custom error message")
+	if result != 0.0 {
+		t.Errorf("Expected result to be 0.0, got %v", result)
+	}
+	if err == nil {
+		t.Error("Expected an error, but got nil")
+	} else if err.Error() != "First custom error message" {
+		t.Errorf("Expected error message to be 'First custom error message', got '%s'", err.Error())
+	}
 }
